5286cbaf7abd73fa1e561d52241ff47a44f1dc70
Alvaro Morilla <almorsan_97@hotmail.com>
2022-09-30 18:50:45 +0200

    Created components
    GraphicsCardListContainer and GraphicsCardDetail

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphicsCardListContainerComponent } from './graphics-card-list-container/graphics-card-list-container.component';
+import { GraphicsCardDetailComponent } from './graphics-card-detail/graphics-card-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphicsCardListContainerComponent,
+    GraphicsCardDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ app/src/app/graphics-card-detail/graphics-card-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphics-card-detail',
+  templateUrl: './graphics-card-detail.component.html',
+  styleUrls: ['./graphics-card-detail.component.scss']
+})
+export class GraphicsCardDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
