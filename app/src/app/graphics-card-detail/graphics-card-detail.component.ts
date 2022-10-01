30f1efc947efb86156384366f7f888009018abf5
Alvaro Morilla <almorsan_97@hotmail.com>
2022-10-01 08:17:28 +0200

    Changed detection strategy to OnPush in components
    + Updated tslib

--- app/src/app/graphics-card-detail/graphics-card-detail.component.ts
+++ app/src/app/graphics-card-detail/graphics-card-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-graphics-card-detail',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './graphics-card-detail.component.html',
   styleUrls: ['./graphics-card-detail.component.scss']
 })
--- app/src/app/graphics-card-list-container/graphics-card-list-container.component.ts
+++ app/src/app/graphics-card-list-container/graphics-card-list-container.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-graphics-card-list-container',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './graphics-card-list-container.component.html',
   styleUrls: ['./graphics-card-list-container.component.scss']
 })
