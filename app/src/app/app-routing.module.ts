1485246da9c974d9f507ed91ae84841f40dad429
Alvaro Morilla <almorsan_97@hotmail.com>
2022-10-04 03:16:04 +0200

    Added routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GraphicsCardDetailComponent } from './graphics-card-detail/graphics-card-detail.component';
+import { GraphicsCardListContainerComponent } from './graphics-card-list-container/graphics-card-list-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/graphics-cards', pathMatch: 'full' },
+  { path: 'graphics-cards', component: GraphicsCardListContainerComponent },
+  { path: 'graphics-cards/:id', component: GraphicsCardDetailComponent },
+  { path: '**', redirectTo: '/graphics-cards' },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
